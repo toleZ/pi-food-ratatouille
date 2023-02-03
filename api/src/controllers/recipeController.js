63b452c2ded57bd3450a0ce7f036c141eb133684
toleZ <toloyjc@gmail.com>
2023-02-03 18:09:23 -0300

    fix

--- api/src/controllers/recipeController.js
+++ api/src/controllers/recipeController.js
@@ -1,6 +1,6 @@
 const { Recipe } = require("../db.js");
 const { API_KEY } = process.env;
-const fetch = require("node-fetch");
+const fetch = import("node-fetch");
 
 const getRecipesDT = async () => {
   const recipes = await Recipe.findAll();
