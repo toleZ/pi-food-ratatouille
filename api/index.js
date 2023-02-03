ffef7816314b7e96036e5335f2d87ef17ce5eba2
toleZ <toloyjc@gmail.com>
2023-02-03 17:30:30 -0300

    fix: env port dynamic

--- api/index.js
+++ api/index.js
@@ -20,11 +20,12 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const { createDiets } = require("./src/utils.js");
+const { PORT } = process.env;
 
 // Syncing all the models at once.
 conn.sync({ alter: true }).then(() => {
-  server.listen(3001, async () => {
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
+  server.listen(PORT, async () => {
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
     await createDiets();
   });
 });
