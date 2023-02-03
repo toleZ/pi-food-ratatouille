7b2b827b0fd758a336d141b9fcc855061ce00336
toleZ <toloyjc@gmail.com>
2023-02-03 17:46:20 -0300

    fix: fetch import module

--- api/src/controllers/recipeController.js
+++ api/src/controllers/recipeController.js
@@ -1,6 +1,6 @@
 const { Recipe } = require("../db.js");
 const { API_KEY } = process.env;
-const fetch = require("node-fetch");
+import fetch from "node-fetch";
 
 const getRecipesDT = async () => {
   const recipes = await Recipe.findAll();
